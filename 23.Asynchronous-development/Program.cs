02482fcb986698e82fc1096c3c8d40bf0a4eb111
Vancho Dimitrov <vancho.dimitrov@outlook.com>
2020-12-21 19:59:56 +0100

    another proejct

---
+++ 23.Asynchronous-development/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _23.Asynchronous_development
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main function thread id: " + Thread.CurrentThread.ManagedThreadId);
+
+            Start();
+
+            Console.ReadKey();
+        }
+
+        static async Task Start()
+        {
+            var obj = new Program();
+            await obj.Method1();
+        }
+
+
+        public async Task Method1()
+        {
+            Console.WriteLine("Method 1 out of Task runs on thread id: " + Thread.CurrentThread.ManagedThreadId);
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine(" runs inside Task, thread ID: " + Thread.CurrentThread.ManagedThreadId);
+            });
+        }
+    }
+}
